e184b7cf69571ae19ada372ead48a0b7e9936a3f
burak_hocaoglu <burak.hocaoglu@data-boss.com.tr>
2022-10-04 14:39:03 +0300

    Repetitive airsim client connection attempts in sweep_motion.py

--- docker/start_task.sh
+++ docker/start_task.sh
@@ -1,3 +1,15 @@
-#!/bin/bash
-set -x
-nohup ./AirSim/build_release/output/bin/RtspStream &
+#!/bin/sh
+
+set -m
+
+echo $HOME
+echo $(pwd)
+echo $(ls $HOME/AirSim/build_release/output/bin)
+
+nohup sh -c $HOME/AirSim/build_release/output/bin/RtspStream &
+
+echo "Sleeping 10 seconds..."
+sleep 10
+
+# echo "-- Motion client"
+# python3 $HOME/AirSim/PythonClient/multirotor/sweep_motion.py 34 7 3 50 15 &
